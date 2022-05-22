adbe200eea6c1bc2354e342ca1f95123b1e8bed4
RoanBrand <brandroan@gmail.com>
2022-05-22 23:48:00 +0200

    graceful exit of go services

--- cmd/Cluster/main.go
+++ cmd/Cluster/main.go
@@ -1,17 +1,38 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"strconv"
 	"sync/atomic"
+	"syscall"
+	"time"
 )
 
 func main() {
 	s := http.Server{Addr: os.Getenv("LISTEN_ADDR")}
 	defer s.Close()
 
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		sig := <-c
+		log.Println("exiting:", sig)
+		if err := stopServer(&s); err != nil {
+			log.Println("error stopping server:", err)
+		}
+	}()
+
+	if err := runServer(&s); err != nil {
+		log.Println("server error:", err)
+	}
+}
+
+func runServer(s *http.Server) error {
 	var count uint64
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -23,5 +44,19 @@ func main() {
 		}
 	})
 
-	log.Println(s.ListenAndServe())
+	err := s.ListenAndServe()
+	if err == http.ErrServerClosed {
+		return nil
+	}
+
+	return err
+}
+
+func stopServer(s *http.Server) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer func() {
+		cancel()
+	}()
+
+	return s.Shutdown(ctx)
 }
--- cmd/RequestCounter/main.go
+++ cmd/RequestCounter/main.go
@@ -7,8 +7,10 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"strconv"
 	"sync/atomic"
+	"syscall"
 	"time"
 
 	"github.com/pkg/errors"
@@ -25,6 +27,23 @@ func main() {
 	s := http.Server{Addr: os.Getenv("LISTEN_ADDR")}
 	defer s.Close()
 
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		sig := <-c
+		log.Println("exiting:", sig)
+		if err := stopServer(&s); err != nil {
+			log.Println("error stopping server:", err)
+		}
+	}()
+
+	if err := runServer(&s, hostName); err != nil {
+		log.Println("server error:", err)
+	}
+}
+
+func runServer(s *http.Server, hostName string) error {
 	var count uint64
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -52,7 +71,21 @@ func main() {
 		}
 	})
 
-	log.Println(s.ListenAndServe())
+	err := s.ListenAndServe()
+	if err == http.ErrServerClosed {
+		return nil
+	}
+
+	return err
+}
+
+func stopServer(s *http.Server) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer func() {
+		cancel()
+	}()
+
+	return s.Shutdown(ctx)
 }
 
 // requestCluster make request to cluster and
